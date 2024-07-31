c7f2c0ce7a011bcbeb40ec6700ab564a2ab8f8de
Rafael Dantas <dantas85.ie@gmail.com>
2024-07-31 19:36:24 +0100

    Activate Budget Items js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Ativar links do menu
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -5,9 +6,19 @@ function ativarLink(link) {
   const href = link.href;
   if(url.includes(href)) {
     link.classList.add("ativo");
-    
   }
 }
 
-
 links.forEach(ativarLink);
+
+//Ativar Itens do Orcamento
+
+const paramentros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  elemento.checked = true;
+  console.log(elemento);
+}
+
+paramentros.forEach(ativarProduto);
