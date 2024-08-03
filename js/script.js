274d8ddcaf4953addf998ddc9435a61f6e9cb08d
Rafael Dantas <dantas85.ie@gmail.com>
2024-08-03 23:58:45 +0100

    update

--- js/script.js
+++ js/script.js
@@ -22,3 +22,29 @@ function ativarProduto(parametro) {
 }
 
 paramentros.forEach(ativarProduto);
+
+// Perguntas Frequentes
+
+const perguntas = document.querySelectorAll('.perguntas button')
+
+
+function ativarPergunta(event) {
+const pergunta = event.currentTarget;
+const controls = pergunta.getAttribute("aria-controls");
+const resposta = document.getElementById(controls)
+
+resposta.classList.toggle("ativa");
+const ativa = resposta.classList.contains("ativa");
+console.log(ativa);
+
+pergunta.setAttribute('aria-expended', ativa);
+
+
+}
+
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener('click', ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
